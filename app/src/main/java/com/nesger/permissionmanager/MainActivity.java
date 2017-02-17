e0965d6ce8c8741383f088d467ff7807ab00841f
nesger <nesger.zhan@gmail.com>
2017-02-17 10:47:32 +0800

    增加多个权限一次性申请操作，只需要简单配置就可以支持多个权限的一次性申请

--- app/src/main/java/com/nesger/permissionmanager/MainActivity.java
+++ app/src/main/java/com/nesger/permissionmanager/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity implements OnPermissionCallb
     private PermissionHelper permissionHelper;
     //值唯一即可,这是为了返回做标识使用
     private final int REQUEST_SETTING = 10;
+    final String[] permissionArrays = {Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_SMS};
+    final int permissionSize = permissionArrays.length;
+    final int[] permissionInfo = {R.string.open_storage_permit,R.string.open_sms_permit};
+    final int infoSize = permissionInfo.length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +36,19 @@ public class MainActivity extends AppCompatActivity implements OnPermissionCallb
         permissionHelper = PermissionHelper.getInstance(this);
         permissionHelper
                 .setForceAccepting(true) // default is false. its here so you know that it exists.
-                .request(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE});
+                .request(permissionArrays);
     }
 
     @Override
     public void onPermissionGranted(@NonNull String[] permissionName) {
         //权限点击允许
+        String lastPermission = permissionName[permissionName.length-1];
+        if (lastPermission.equals(permissionArrays[permissionSize-1])){
+            //权限点击允许
+            doYourThings();
+        }else{
+            doPermissionCheck();
+        }
     }
 
     @Override
@@ -48,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements OnPermissionCallb
     @Override
     public void onPermissionPreGranted(@NonNull String permissionsName) {
         //权限已经打开了
+        doYourThings();
     }
 
     @Override
@@ -58,20 +70,39 @@ public class MainActivity extends AppCompatActivity implements OnPermissionCallb
 
     @Override
     public void onPermissionReallyDeclined(@NonNull String permissionName) {
+        boolean admitAppend = false;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0,length=permissionArrays.length;i<length;i++){
+            if (permissionArrays[i].equals(permissionName) || admitAppend){
+                if (i<infoSize){
+                    sb.append(getString(permissionInfo[i]));
+                    admitAppend = true;
+                }
+            }
+            if (i == length - 1){
+                if ("".equals(sb.toString())){
+                    Toast.makeText(MainActivity.this,R.string.open_permit,Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,sb.toString(),Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                if (!"".equals(sb.toString())){
+                    sb.append("\n");
+                }
+            }
+        }
         //禁止权限
         Intent intent = new Intent();
         intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         Uri uri = Uri.parse("package:" + getPackageName());
         intent.setData(uri);
-        startActivityForResult(intent,REQUEST_SETTING);
-        //TODO 跳转前可以做下简单的提示,下面的提示是简单的一个示例
-        Toast.makeText(this,"请打开存储权限",Toast.LENGTH_SHORT).show();
+        startActivityForResult(intent, REQUEST_SETTING);
     }
 
     //android6.0以下会触发
     @Override
     public void onNoPermissionNeeded() {
-
+        doYourThings();
     }
 
     @Override
@@ -92,5 +123,9 @@ public class MainActivity extends AppCompatActivity implements OnPermissionCallb
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         permissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void doYourThings(){
+        Toast.makeText(this,"权限全部打开完毕",Toast.LENGTH_SHORT).show();
     }
 }
